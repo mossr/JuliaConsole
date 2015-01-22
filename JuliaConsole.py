a27c2ae957407b59ed13adc92c41ff7f57fd9712
Robert Moss <mossr@wit.edu>
2015-01-22 17:52:36 -0500

    Send selected text to a Console2 julia tab and evaluate.

--- JuliaConsole.py
+++ JuliaConsole.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sublime, sublime_plugin, os.path, subprocess
 
 console2 = "C:\Program Files\Console2\Console.exe"
@@ -13,3 +15,12 @@ class Console2Command(sublime_plugin.TextCommand):
 		f = self.view.file_name()
 		cmds = [console2, "-d", os.path.dirname(f)]
 		process = subprocess.Popen(cmds)
+
+class JuliaEvalStringCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sels = self.view.sel()
+		for sel in sels:
+			jl_cmd = self.view.substr(sel).replace("\n", ";").replace("\"", "\\\"")
+			command = [console2, '-t', 'julia', '-r', "-P \"%s\"" % jl_cmd]
+			print command
+			process = subprocess.Popen(command)
