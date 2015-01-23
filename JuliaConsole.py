493ec3b14187432df12d04c007c09cfc3881bd02
Robert Moss <mossr@wit.edu>
2015-01-22 20:15:19 -0500

    Added sublime-settings file. Added option to output to the Sublime console (ctrl `). Ability to load current Julia file in a command window

--- JuliaConsole.py
+++ JuliaConsole.py
@@ -2,25 +2,42 @@
 # -*- coding: utf-8 -*-
 import sublime, sublime_plugin, os.path, subprocess
 
-console2 = "C:\Program Files\Console2\Console.exe"
+settings                = sublime.load_settings("JuliaConsole.sublime-settings")
+console2_location       = settings.get('console_location').encode("ascii")
+console2_julia_tab_name = settings.get('console2_julia_tab_name').encode("ascii")
+julia_location          = settings.get('julia_location').encode("ascii")
 
 class JuliaConsoleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		f = self.view.file_name()
-		cmds = [console2, "-t", "julia", "-d", os.path.dirname(f)]
+		cmds = [console2_location, "-t", console2_julia_tab_name, "-d", os.path.dirname(f)]
 		process = subprocess.Popen(cmds)
 
 class Console2Command(sublime_plugin.TextCommand):
 	def run(self, edit):
 		f = self.view.file_name()
-		cmds = [console2, "-d", os.path.dirname(f)]
+		cmds = [console2_location, "-d", os.path.dirname(f)]
 		process = subprocess.Popen(cmds)
 
 class JuliaEvalStringCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		useSublimeConsole = settings.get('output_to_sublime_console')
 		sels = self.view.sel()
 		for sel in sels:
-			jl_cmd = self.view.substr(sel).replace("\n", ";").replace("\"", "\\\"")
-			command = [console2, '-t', 'julia', '-r', "-P \"%s\"" % jl_cmd]
-			print command
-			process = subprocess.Popen(command)
+			jl_cmd = self.view.substr(sel).replace("\n", ";").replace("\"", "\\\"").replace("\\", "\\\\").encode("ascii")
+			if useSublimeConsole:
+				command = [julia_location, '-E', "%s" % jl_cmd]
+				pipe = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+				output = str.join("", pipe.stdout.readlines())
+				sts = pipe.wait()
+				print output
+			else:
+				command = [console2_location, '-t', console2_julia_tab_name, '-r', "-P \"%s\"" % jl_cmd]
+				process = subprocess.Popen(command)
+
+
+class JuliaReloadCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		f = self.view.file_name()
+		command = [console2_location, '-t', console2_julia_tab_name, '-r', "-L %s" % os.path.abspath(os.path.basename(f)).encode("ascii")]
+		process = subprocess.Popen(command)
